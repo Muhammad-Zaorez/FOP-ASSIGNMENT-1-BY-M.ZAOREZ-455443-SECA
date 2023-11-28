ac8b7a8fd23b3caf2936dca98c2ac4ed6ce0456d
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-11-28 10:51:19 -0800

    Add files via upload

---
+++ Lab assignment 1 by M.Zaorez.cpp
@@ -0,0 +1,66 @@
+/*
+                                    ASSIGNMENT 01
+                                    M.ZAOREZ(455443)
+                                        SEC-A
+                            SUBMITTED TO: SIR JAWAD KHAN
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	//                                 TASK 01
+	/*int n,factor;
+	cout<<"enter number: ";
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		if(n%i==0){	
+		cout<<i<<",";
+		}
+	}
+/*                                     TASK 02
+THE OUTPUT OF THE TASK WILL BE X IS 5 AND Y IS 10 BECAUSE THE INTEGERS ARE DEFINED BY DEFAULT.	
+*/
+//                                     TASK 03
+   /*int n=0;
+   cout<<"enter number: ";
+   cin>>n;
+    if(n>10&&n<=20){
+	   cout<<"1";
+}
+    else
+    cout<<"0";*/
+    //                              TASK 04
+    // UNABLE TO PERFORM SORRY
+    //                              TASK 05
+    // UNABLE TO PERFORM SORRY
+    //                             TASK 06
+    /*int dividend,divisor,i;
+    cout<<"enter dividend: ";
+    cin>>dividend;
+    cout<<"enter divisor: ";
+    cin>>divisor;
+    while(dividend>=divisor){
+    	divisor=dividend+i;
+    	i++;
+	}
+	cout<<"quotient is: "<<i;*/
+
+//                                 TASK 08
+ /*int a[8] = {1, 2, 3, 4, 5};
+
+    a[5] = 6;
+    a[6] = 7;
+    a[7] = 8;
+
+    for (int i = 0; i < 8; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;*/
+    
+//                                          TASK 09
+
+// DONE ON ONLINE COMPILER SS ATTACHED 
+}
+
+
